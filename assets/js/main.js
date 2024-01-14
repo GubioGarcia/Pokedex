b1c3389cef7a34d3a0dbc140e2c0241ffcf67f0b
Gubio Garcia <gubiogarcia@gmail.com>
2024-01-14 16:37:31 -0300

    Refactor: manipulando multiplas requisições em paralelo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,22 @@ const offset = 0
 const limit = 12
 const url =  `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToHtlm(pokemon) {
  return `
     <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">#${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
+                ${convertPokemonTypesToLi(pokemon.types).join('')}
             </ol>
 
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+            <img src="${pokemon.sprites.other.dream_world.front_default}" 
                 alt="${pokemon.name}">
         </div>
     </li>
@@ -25,10 +28,5 @@ function convertPokemonToHtlm(pokemon) {
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    const newList = pokemons.map((pokemon) => {
-        return convertPokemonToHtlm(pokemon)
-    })
-
-    const newHtml = newList.join('')
-    pokemonList.innerHTML += newHtml
+    pokemonList.innerHTML += pokemons.map(convertPokemonToHtlm).join('')
 })
